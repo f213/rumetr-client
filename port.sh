904c4bc842a7282c87fc54b71ede4cb8fa77195b
Fedor Borshev <f@f213.in>
2017-08-07 14:18:22 +0300

    Script for easily copying the client library to the another virtual env

---
+++ port.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copy the current branch to the destination venv
+#
+
+d=`pwd`
+dst=$1
+
+if [ -z $dst]; then
+    echo "Usage: ${0} <destination env>"
+    exit 127
+fi
+
+. venv/bin/activate
+/bin/rm -Rf dist
+python setup.py sdist
+/bin/cp dist/roometr-client-*.tar.gz /tmp/
+. $dst/venv/bin/activate
+echo Y|pip uninstall roometr-client
+cd /tmp
+/usr/bin/tar zxpvf roometr-client-*.tar.gz
+cd roometr-client-*
+easy_install .
+
+cd $d
