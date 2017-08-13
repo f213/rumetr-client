b236b210d4e5041508faae96694bdbefc2b27ec6
Fedor Borshev <f@f213.in>
2017-08-13 10:45:46 +0300

    Bundled pipeline for uploading items scraped by scrapy

---
+++ roometr/scrapy.py
@@ -0,0 +1,80 @@
+from roometr import Roometr
+
+
+class UploadPipeline(object):
+    _client = None
+
+    def process_item(self, item, spider):
+        self.spider = spider
+        self.item = item
+
+        self.add_complex_if_required()
+        self.add_house_if_required()
+        self.update_appt()
+
+    def add_complex_if_required(self):
+        if not self.c.complex_exists(self.item['complex_id']):
+            self.c.add_complex(
+                external_id=self.item['complex_id'],
+                name=self.item['complex_name'],
+                url=self.item['complex_url'],
+            )
+
+    def add_house_if_required(self):
+        if not self.c.house_exists(self.item['complex_id'], self.item['house_id']):
+            house = dict(
+                complex=self.item['complex_id'],
+                external_id=self.item['house_id'],
+                name=self.item['house_name'],
+                url=self.item['house_url'],
+            )
+            if self.item['house_addr'] is not None and len(self.item['house_addr']):
+                house['address'] = {
+                    'value': self.item['house_addr'],
+                }
+
+            self.c.add_house(**house)
+
+    def update_appt(self):
+        appt = dict(
+            complex=self.item['complex_id'],
+            house=self.item['house_id'],
+            external_id=self.item['id'],
+            floor=self.item['floor'],
+            room_count=self.item['room_count'],
+            square=self.item['square'],
+            price=self.item['price'],
+        )
+        if not self.c.appt_exists(self.item['complex_id'], self.item['house_id'], self.item['id']):
+            self.c.add_appt(**appt)
+        else:
+            self.c.update_appt(**appt)
+
+    @property
+    def c(self):
+        """
+        Caching client for not repeapting checks
+        """
+        if self._client is None:
+            self._parse_settings()
+            self._client = Roometr(**self.settings)
+        return self._client
+
+    def _parse_settings(self):
+        if hasattr(self, 'settings'):  # parse setting only one time
+            return
+
+        self.settings = {
+            'auth_key': self._check_required_setting('RUMETR_TOKEN'),
+            'developer': self._check_required_setting('RUMETR_DEVELOPER'),
+        }
+        self.settings.update(self._non_required_settings('RUMETR_API_HOST'))
+
+    def _check_required_setting(self, setting) -> str:
+
+        if setting not in self.spider.settings.keys() or not len(self.spider.settings[setting]):
+            raise TypeError('Please set up %s in your scrapy settings' % setting)
+        return self.spider.settings[setting]
+
+    def _non_required_settings(self, *args) -> dict:
+        return {setting.replace('RUMETR_', '').lower(): self.spider.settings[setting.upper()] for setting in args if setting in self.spider.settings.keys()}
